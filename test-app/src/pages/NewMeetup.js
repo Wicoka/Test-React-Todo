08334d7862c18991eb75522111782cef2a21c0b2
Szőke Viktor <viktor.szoke@schdesign.hu>
2021-05-30 15:54:47 +0200

    Minor fixes

--- test-app/src/pages/NewMeetup.js
+++ test-app/src/pages/NewMeetup.js
@@ -1,9 +1,15 @@
+import { useHistory, } from 'react-router-dom';
 import NewMeetupForm from '../components/meetups/NewMeetupFrom';
 
 function NewMeetupPage(props) {
+    // Ez kell hozzá, hogy mondjuk elnavigáljon egy másik oldalra kódból
+    const history = useHistory();
+
     function addMeetupHandler(meetupData) {
         // Itt akarom elküldeni a HTTP kérést ha új Meetup-ot adok hozzá az alkalmazásomhoz
 
+        // Ez visszanavigál az eredeti oldalra
+        history.replace('/');
     }
 
     return (
