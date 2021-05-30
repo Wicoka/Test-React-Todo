e33376b907b3fa12d87c6113e1060ee8ee7bbf22
Szőke Viktor <viktor.szoke@schdesign.hu>
2021-05-30 11:17:23 +0200

    Add Links and Navigation layout

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -3,10 +3,12 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       {/* 
           A switch azért kell, hogy a / és a /favorites például ne egyszerre renderelődjön ki
           A Swiwtch nélkül a /favorites-re mindkettő kirenderelődne.
