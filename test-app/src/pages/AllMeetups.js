86efad08179bca50820dd2d4f84136af342a637a
Szőke Viktor <viktor.szoke@schdesign.hu>
2021-05-30 11:51:01 +0200

    Listing like ngFor

--- test-app/src/pages/AllMeetups.js
+++ test-app/src/pages/AllMeetups.js
@@ -1,6 +1,24 @@
+const DUMMY_DATA = [
+    { id: 1, title: 'Meetup 1' },
+    { id: 2, title: 'Meetup 2' },
+    { id: 3, title: 'Meetup 3' },
+    { id: 4, title: 'Meetup 4' }
+];
+
 function AllMeetupsPage(props) {
     return (
-        <div>AllMeetupsPage</div>
+        <section>
+            <h1>All Meetups Page</h1>
+            <ul>
+                {DUMMY_DATA.map((meetup) => {
+                    return (
+                        <li>
+                            {meetup.title}
+                        </li>
+                    );
+                })}
+            </ul>
+        </section>
     );
 }
 
